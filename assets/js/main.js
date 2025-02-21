dca06907ea3acbb1c5811813efa5cdbc1b26ebea
JADHAV AMOL <134080298+JadhaAM@users.noreply.github.com>
2025-02-21 21:23:51 +0530

    add the google form

--- assets/js/main.js
+++ assets/js/main.js
@@ -441,46 +441,7 @@
         '[name="name"],[name="email"],[name="subject"],[name="number"],[name="message"]'; // Must be use (,) without any space
     var formMessages = $(".form-messages");
 
-    function sendContact() {
-        var formData = $(form).serialize();
-        var valid;
-        valid = validateContact();
-        if (valid) {
-            jQuery
-                .ajax({
-                    url: $(form).attr("action"),
-                    data: formData,
-                    type: "POST",
-                })
-                .done(function (response) {
-                    // Make sure that the formMessages div has the 'success' class.
-                    formMessages.removeClass("error");
-                    formMessages.addClass("success");
-                    // Set the message text.
-                    formMessages.text(response);
-                    // Clear the form.
-                    $(
-                        form +
-                            ' input:not([type="submit"]),' +
-                            form +
-                            " textarea"
-                    ).val("");
-                })
-                .fail(function (data) {
-                    // Make sure that the formMessages div has the 'error' class.
-                    formMessages.removeClass("success");
-                    formMessages.addClass("error");
-                    // Set the message text.
-                    if (data.responseText !== "") {
-                        formMessages.html(data.responseText);
-                    } else {
-                        formMessages.html(
-                            "Oops! An error occured and your message could not be sent."
-                        );
-                    }
-                });
-        }
-    }
+    
 
     function validateContact() {
         var valid = true;
@@ -1336,4 +1297,7 @@
     //     }
     //   }
     
-})(jQuery);
+})(jQuery); 
+$(document).ready(function() {
+    sendContact();
+});
