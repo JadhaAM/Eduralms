1869ecaa0e9819415fb639d4ae5f9dc3ad413426
JADHAV AMOL <134080298+JadhaAM@users.noreply.github.com>
2025-02-26 02:21:32 +0530

    update the vercal.json file

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/bash 
+# Create public directory if it doesn't exist
+mkdir -p public
+
+# Copy all your HTML, CSS, JS files to the public directory
+cp -r *.html *.css *.js images/ public/ 
+
 echo "window.env = {
   apiKey: '$API_KEY',
   authDomain: '$AUTH_DOMAIN',
